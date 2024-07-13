45a9451e3005d8a71bfd487950c0c04316c9970d
Rhyme Bulbul <rhyme1325@gmail.com>
2024-07-13 18:48:34 +1000

    Add POST API

--- src/main/java/com/example/tommy/HelloResource.java
+++ src/main/java/com/example/tommy/HelloResource.java
@@ -1,8 +1,10 @@
 package com.example.tommy;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.PathParam;
 
 @Path("/hello-world")
 public class HelloResource {
@@ -10,5 +12,11 @@ public class HelloResource {
     @Produces("text/plain")
     public String hello() {
         return "Hello, World!";
+    }
+    @POST
+    @Path("/{name}")
+    @Produces("text/plain")
+    public String customWelcome(@PathParam("name") String name) {
+           return "Welcome " + name + "!";
     }
 }
